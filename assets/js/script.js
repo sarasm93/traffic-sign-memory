52a35508e87e9937ff59c8c5a527781094cd2342
Sara Smith <saraa.smith@hotmail.com>
2023-04-15 14:27:35 +0000

    Add functionality to game board by adding flip functions and conditions for flipping

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,16 @@
 document.addEventListener("DOMContentLoaded", function() { 
 
 let cards = document.getElementsByTagName("img");
-let resetButton = document.getElementById("reset");
-let saveButton = document.getElementById("submit");
-let firstImage = null;
-let secondImage = null;
-let placeholder = ["assets/images/placeholder-car.png"];
+let playButton = document.getElementById("play");
+let save = document.getElementById("submit");
+let firstCard = null;
+let secondCard = null;
+let time = 0;
+let timerStarted = false;
+let timeInterval = null;
+let matchesCounter = 0;
+let boardBlocked = false;
+
 
 // Image pairs to replace placeholder image when img-elements in html are clicked.
 let imageSrc = [
@@ -34,7 +39,7 @@ let imageSrc = [
 ];
 
 
-// The code on lines 39-50 is taken from a Stackoverflow.com page, this page: 
+// The code on lines 41-52 is taken from a Stackoverflow.com page, this page: 
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 // A function to shuffle the images on the game board by creating two variables (one with a value of a random number) 
 // and use them to set and swap the indexes of the imageSrc-array elements.
@@ -53,44 +58,84 @@ function shuffle(array) {
 
 shuffle(imageSrc);
 
-for (let image of imageSrc) {
-    cards.innerHTML = '${imageSrc[image]}';
-    console.log(cards);
+// The code on lines 58-67 has been copied from this Stackoverflow.com page (see Answer 2 in the link below) and then 
+// modified slightly by changing the code and adding some new code.
+// https://stackoverflow.com/questions/37187504/javascript-second-counter
+function timer() {
+    timeInterval = setInterval(function (){
+        document.getElementById('time').innerHTML = time;
+        time++;
+    }, 1000);
 };
+
+
+function flipCard(event){
+    const card = event.target;
+    const arrayIndex = parseInt(card.getAttribute('data-attribute'));
+    console.info(`Flip index ${arrayIndex}`);
+    card.src = imageSrc[arrayIndex];
+}
+
+function unFlipCards(firstCard, secondCard){
+    console.info('should unflip');
+    setTimeout(function(){
+        firstCard.src = 'assets/images/placeholder-car.png';
+        secondCard.src = 'assets/images/placeholder-car.png';
+        boardBlocked = false;
+    }, 1500);
+    
+}
+
+
+function userPlay(event){
+    if (boardBlocked === false){
+        flipCard(event);
+        if (firstCard === null){
+            firstCard = event.target;
+            if (timerStarted === false){
+                timer();
+                timerStarted = true;
+            }
+        }
+        else{
+            boardBlocked = true;
+            if (secondCard === null){
+                secondCard = event.target;
+                if (secondCard.src === firstCard.src){
+                    matchesCounter++;
+                    console.info(`Its a match ${matchesCounter}`);
+                    firstCard.removeEventListener('click', userPlay);
+                    secondCard.removeEventListener('click', userPlay);
+                    boardBlocked = false;
+                    if(matchesCounter >= 10){
+                        clearInterval(timeInterval);
+                        setTimeout(function(){
+                            alert(`You won in ${time} seconds`);
+                        }, 1000)
+                    }
+                }
+                else{
+                    console.info('Its NOT a match');
+                    unFlipCards(firstCard, secondCard);
+                }
+                firstCard = null;
+                secondCard = null;
+            }
+        }
+    }
+}
 
 
 
 // Used this link to understand how to change src-attribute for images: 
 // https://stackoverflow.com/questions/11722400/programmatically-change-the-src-of-an-img-tag
+
 let index = 0;
 for (let card of cards) {
-    card.src.onload = placeholder[0];
-    card.src = imageSrc[index];
-    index++;
-    card.addEventListener("click", function(event) {
-        console.info(event.target.src);
-        if (firstImage === null){
-            firstImage = event.target.src;
-        }
-        else{
-            if (secondImage === null){
-                secondImage = event.target.src;
-
-                if (secondImage === firstImage){
-                    console.info('Its a match');
-                }
-                else{
-                    console.info('Its NOT a match');
-                }
-                firstImage = null;
-                secondImage = null;
-            }
-        }
-    });
-    
+    // card.src = imageSrc[index];
+    // index++;
+    card.addEventListener("click", userPlay);
 }
-//Reset game
-
 
 /* index of imageSrc should match a random number. 
 The random number should be the value of the data-attribute on the img-tags.
@@ -101,7 +146,6 @@ the data-attribute of the img-tags should get at random number.
 The random number should match the index of the items in the imageSrc array.
 When a card/image is clicked the src-attribute of the img should be changed to 
 the the string in the imageSrc-array that has the */
-
 
 
 });

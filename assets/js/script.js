0dbbf7d2e112baec7658b7b1c796aa706782232c
Sara Smith <saraa.smith@hotmail.com>
2023-04-16 16:52:18 +0000

    Add changes to background color of cards when they are flipped and unflipped

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,122 +2,127 @@
 // https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+LM101+2021_T1/courseware/2d651bf3f23e48aeb9b9218871912b2e/78f3c10a937c4fe09640c7c0098d16bd/?child=first
 document.addEventListener("DOMContentLoaded", function() { 
 
-let cards = document.getElementsByTagName("img");
-let playButton = document.getElementById("play");
-let save = document.getElementById("submit");
-let firstCard = null;
-let secondCard = null;
-let time = 0;
-let timerStarted = false;
-let timeInterval = null;
-let matchesCounter = 0;
-let boardBlocked = false;
-
-
-// Image pairs to replace placeholder image when img-elements in html are clicked.
-let imageSrc = [
-    "assets/images/bicycle-lane.png",
-    "assets/images/bicycle-lane.png", 
-    "assets/images/blind-alley.png",
-    "assets/images/blind-alley.png", 
-    "assets/images/motorway.png",
-    "assets/images/motorway.png", 
-    "assets/images/no-overtaking.png",
-    "assets/images/no-overtaking.png", 
-    "assets/images/no-stopping.png",
-    "assets/images/no-stopping.png", 
-    "assets/images/priority-road.png",
-    "assets/images/priority-road.png",
-    "assets/images/quayside-warning.png",
-    "assets/images/quayside-warning.png",
-    "assets/images/road-narrows.png",
-    "assets/images/road-narrows.png",
-    "assets/images/slippery-road.png",
-    "assets/images/slippery-road.png",
-    "assets/images/stop.png",
-    "assets/images/stop.png",
-];
-
-
-// The code on lines 41-52 is taken from this Stackoverflow.com page: 
-// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-// A function to shuffle the images on the game board by creating two variables (one with a value of a random number) 
-// and use them to set and swap the indexes of the imageSrc-array elements.
-function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
-    while (currentIndex != 0) {
-
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+    let cards = document.getElementsByTagName("img");
+    let playButton = document.getElementById("play");
+    let saveButton = document.getElementById("submit");
+    let firstCard = null;
+    let secondCard = null;
+    let time = 0;
+    let timerStarted = false;
+    let timeInterval = null;
+    let matchesCounter = 0;
+    let boardBlocked = false;
+    
+    
+    // Image pairs to replace placeholder image when img-elements in html are clicked.
+    let imageSrc = [
+        "assets/images/bicycle-lane.png",
+        "assets/images/bicycle-lane.png", 
+        "assets/images/blind-alley.png",
+        "assets/images/blind-alley.png", 
+        "assets/images/motorway.png",
+        "assets/images/motorway.png", 
+        "assets/images/no-overtaking.png",
+        "assets/images/no-overtaking.png", 
+        "assets/images/no-stopping.png",
+        "assets/images/no-stopping.png", 
+        "assets/images/priority-road.png",
+        "assets/images/priority-road.png",
+        "assets/images/quayside-warning.png",
+        "assets/images/quayside-warning.png",
+        "assets/images/road-narrows.png",
+        "assets/images/road-narrows.png",
+        "assets/images/slippery-road.png",
+        "assets/images/slippery-road.png",
+        "assets/images/stop.png",
+        "assets/images/stop.png",
+    ];
+    
+    
+    // The code on lines 41-52 is taken from a Stackoverflow.com page, this page: 
+    // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    // A function to shuffle the images on the game board by creating two variables (one with a value of a random number) 
+    // and use them to set and swap the indexes of the imageSrc-array elements.
+    function shuffle(array) {
+        let currentIndex = array.length, randomIndex;
+        while (currentIndex != 0) {
+      
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+        return array;
     }
-    return array;
-};
-
-shuffle(imageSrc);
-
-// Time counter counting the seconds it takes for the user to find all matching pairs and then stops.
-function timer() {
-    timeInterval = setInterval(function() {
-        document.getElementById('time').innerHTML = time;
-        time++;
-    }, 1000);
-};
-
-// Functions to flip the cards 
-function flipCard(event) {
-    const card = event.target;
-    const arrayIndex = parseInt(card.getAttribute('data-attribute'));
-    console.info(`Flip index ${arrayIndex}`);
-    card.src = imageSrc[arrayIndex];
-};
-
-function unFlipCards(firstCard, secondCard) {
-    console.info('should unflip');
-    setTimeout(function() {
-        firstCard.src = 'assets/images/placeholder-car.png';
-        secondCard.src = 'assets/images/placeholder-car.png';
-        boardBlocked = false;
-    }, 1500);
-};
-
-
-function userPlay(event) {
-    if (boardBlocked === false) {
-        flipCard(event);
-        if (firstCard === null) {
-            firstCard = event.target;
-            if (timerStarted === false) {
-                timer();
-                timerStarted = true;
-            }
-        } else {
-            boardBlocked = true;
-            if (secondCard === null) {
-                secondCard = event.target;
-                if (secondCard.src === firstCard.src) {
-                    matchesCounter++;
-                    console.info(`Its a match ${matchesCounter}`);
-                    firstCard.removeEventListener('click', userPlay);
-                    secondCard.removeEventListener('click', userPlay);
-                    boardBlocked = false;
-                    if (matchesCounter >= 10) {
-                        clearInterval(timeInterval);
-                        setTimeout(function() {
-                            alert(`You won in ${time} seconds`);
-                        }, 1000)
-                    }
-                } else {
-                    console.info('Its NOT a match');
-                    unFlipCards(firstCard, secondCard);
+    
+    shuffle(imageSrc);
+    
+    // The code on lines 58-67 has been copied from this Stackoverflow.com page (see Answer 2 in the link below) and then 
+    // modified slightly by changing the code and adding some new code.
+    // https://stackoverflow.com/questions/37187504/javascript-second-counter
+    function timer() {
+        timeInterval = setInterval(function (){
+            document.getElementById('time').innerHTML = time;
+            time++;
+        }, 1000);
+    };
+    
+    
+    function flipCard(event){
+        const card = event.target;
+        const arrayIndex = parseInt(card.getAttribute('data-attribute'));
+        console.info(`Flip index ${arrayIndex}`);
+        card.src = imageSrc[arrayIndex];
+        card.style.backgroundColor = "white";
+    }
+    
+    function unFlipCards(firstCard, secondCard){
+        console.info('should unflip');
+        setTimeout(function(){
+            firstCard.src = 'assets/images/placeholder-car.png';
+            secondCard.src = 'assets/images/placeholder-car.png';
+            boardBlocked = false;
+            firstCard.style.backgroundColor = "blue";
+            secondCard.style.backgroundColor = "blue";
+        }, 1500);
+    }
+    
+    function userPlay(event){
+        if (boardBlocked === false){
+            flipCard(event);
+            if (firstCard === null){
+                firstCard = event.target;
+                if (timerStarted === false){
+                    timer();
+                    timerStarted = true;
                 }
-                firstCard = null;
-                secondCard = null;
+            }
+            else{
+                boardBlocked = true;
+                if (secondCard === null){debugger
+                    secondCard = event.target;
+                    if (secondCard.src === firstCard.src){
+                        matchesCounter++;
+                        console.info(`Its a match ${matchesCounter}`);
+                        firstCard.removeEventListener('click', userPlay);
+                        secondCard.removeEventListener('click', userPlay);
+                        boardBlocked = false;
+                        if(matchesCounter >= 10){
+                            clearInterval(timeInterval);
+                            setTimeout(function(){
+                                alert(`You won in ${time} seconds`);
+                            }, 200)
+                        }
+                    }
+                    else{
+                        console.info('Its NOT a match');
+                        unFlipCards(firstCard, secondCard);
+                    }
+                    firstCard = null;
+                    secondCard = null;
+                }
             }
         }
     }
-}
 });
-
